57c27222c359b42cd65e112d1b6dcc44df30a71c
yedou <bRzf_4UAZ2cE9whhrPgAU9>
2019-09-12 09:46:11 +0800

    解析

--- app/src/main/java/com/is/myshare/MainActivity.java
+++ app/src/main/java/com/is/myshare/MainActivity.java
@@ -1,37 +1,40 @@
 package com.is.myshare;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.is.myshare.tome.BaseShareData;
-import com.is.myshare.tome.ShareToMe;
-import com.is.myshare.tome.ShareToMeActivity;
 
-public class MainActivity extends ShareToMeActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.DBoy.share.to.me.BaseShareData;
+import com.DBoy.share.to.me.ShareToMe;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        ShareToMe.handleShareToMe(this,getIntent(), new ShareToMe.HandleListener() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ShareToMe.handleShareToMe(this, getIntent(), new ShareToMe.HandleListener() {
             @Override
             public void handleType(String type) {
-                Log.d("DDDd","type"+type);
+                Log.d("Dboy", "type =>" + type);
             }
 
             @Override
             public void handleContent(BaseShareData shareData) {
-                Log.d("DDDd","data"+shareData.toString());
+                Log.d("Dboy", "data =>" + shareData.toString());
             }
 
             @Override
             public void handleError(String e) {
-                Log.d("DDDd","error = "+e);
+                Log.d("Dboy", "error =>" + e);
             }
         });
-
-
     }
 }
--- app/src/main/java/com/is/myshare/tome/ShareToMeActivity.java
+++
@@ -1,7 +0,0 @@
-package com.is.myshare.tome;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class ShareToMeActivity extends AppCompatActivity {
-
-}
